bb3671b3fa5cdc470950160ef47669811a79c37b
viniciushenrique <viniciushenrique02@hotmail.com>
2024-08-27 17:40:32 -0300

    atividades

---
+++ java/curso-udemy/POO/Atividade2/src/application/App.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Scanner;
+import entities.Employee;
+import java.util.Locale;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee emp = new Employee(); 
+
+        System.out.print("Name: ");
+        emp.name = sc.nextLine();
+        System.out.print("Gross salary: ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.print("tax: ");
+        emp.tax = sc.nextDouble();
+        
+    }
+}
